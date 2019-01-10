00b649e6a7386449f6ab13d78ba3fc29e3b4d2b4
Oleksii Obludskyi <Obludsky@ukr.net>
2019-01-11 00:39:27 +0200

    Create login template and routes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app.routing';
-import { LayoutComponent } from './components/layout/layout.component';
+import { SimpleLayoutComponent } from './components/simple-layout/simple-layout.component';
+import { NavLayoutComponent } from './components/nav-layout/nav-layout.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent,
+    SimpleLayoutComponent,
+    NavLayoutComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import {Routes, RouterModule, PreloadAllModules} from '@angular/router';
-import { LayoutComponent } from './components/layout/layout.component';
+import { SimpleLayoutComponent } from './components/simple-layout/simple-layout.component';
+import {NavLayoutComponent} from './components/nav-layout/nav-layout.component';
 
 export const routes: Routes = [
-  { path: '', component: LayoutComponent },
-  { path: 'login', loadChildren: './components/login/login.module#LoginModule' },
+  {path: '', redirectTo: 'nav', pathMatch: 'full'},
+  {
+    path: '',
+    component: SimpleLayoutComponent,
+    children: [
+      { path: 'login', loadChildren: './components/login/login.module#LoginModule' },
+    ],
+  },
+  {
+    path: 'nav',
+    component: NavLayoutComponent,
+  },
+
   // otherwise redirect to NotFoundPage
 ];
 
